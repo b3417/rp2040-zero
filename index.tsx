10f2dfa2dd3f1844e1e669e71f8afddeba425678
baeltaezaer <info@convierteweb.com.mx>
2026-06-10 23:56:19 -0600

    Use common Xiao RP2040 board

--- index.tsx
+++ index.tsx
@@ -1,18 +1,44 @@
+import { XiaoBoard } from "@tscircuit/common"
 import { VoltageRegulator } from "./lib/VoltageRegulator"
-import { RP2040 } from "./imports/RP2040"
-import { PinOutCircuit } from "./lib/PinOutCircuit"
 import { LedCircuit } from "./lib/LedCircuit"
 import { FlashCircuit } from "./lib/FlashCircuit"
 import { CrystalCircuit } from "./lib/CrystalCircuit"
 import { RP2040Circuit } from "./lib/RP2040Circuit"
 
 export default () => (
-  <board routingDisabled schMaxTraceDistance={5}>
+  <XiaoBoard
+    variant="RP2040"
+    boardProps={{ routingDisabled: true, schMaxTraceDistance: 5 }}
+    chipProps={{
+      name: "P1",
+      connections: {
+        SWDIO: "net.SWD",
+        SWCLK: "net.SWCLK",
+        RUN: "net.RUN",
+        GND1: "net.GND",
+        GND2: "net.GND",
+        GND3: "net.GND",
+        VIN: "net.VSYS",
+        VBUS: "net.USB_VDD",
+        V3_3: "net.V3_3",
+        A0: "net.GPIO26",
+        A1: "net.GPIO27",
+        A2: "net.GPIO28",
+        A3: "net.GPIO29",
+        SDA: "net.GPIO6",
+        SCL: "net.GPIO7",
+        TX: "net.GPIO0",
+        RX: "net.GPIO1",
+        SCK: "net.GPIO2",
+        MOSI: "net.GPIO3",
+        MISO: "net.GPIO4",
+      },
+    }}
+  >
     <VoltageRegulator />
-    <PinOutCircuit />
     <LedCircuit />
     <FlashCircuit />
     <CrystalCircuit />
     <RP2040Circuit />
-  </board>
+  </XiaoBoard>
 )
